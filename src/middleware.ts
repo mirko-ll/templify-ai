5d6eea83834e379f17907c2cde671c3786523fb4
Mirko Cosic <mirko.logiklabs@gmail.com>
2025-08-11 14:54:00 +0200

    Middleware setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { withAuth } from "next-auth/middleware"
 
 export default withAuth(
-  function middleware(req) {
+  function middleware() {
     // Add any additional middleware logic here
   },
   {
@@ -20,14 +20,6 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api/auth (auth API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - auth/signin (sign in page)
-     */
-    "/((?!api/auth|_next/static|_next/image|favicon.ico|auth/signin).*)",
+    "/",
   ],
 }
